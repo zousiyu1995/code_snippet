7b5f6b4e73a20e55a9300414c1b8a2730269200d
Zousiyu <zousiyu1995@outlook.com>
2017-08-19 14:16:30 +0800

    添加git章节，对unfold.py进行了一次优雅的修正

--- code/python/unfold.py
+++ code/python/unfold.py
@@ -1,12 +1,23 @@
 from collections import Iterable
 
+#def unfold(items, unfolded=None, ignore_types=(str, bytes)):
+#    unfolded = list() if unfolded is None else unfolded
+#
+#    for item in items:
+#        if isinstance(item, Iterable) and not isinstance(item, ignore_types):
+#            unfold(item, unfolded=unfolded)
+#        else:
+#            unfolded.append(item)
+#
+#    return unfolded
 
-def unfold(items, unfolded=None, ignore_types=(str, bytes)):
-    unfolded = list() if unfolded is None else unfolded
 
+# 更优雅的版本
+def unfold(items, ignore_types=(str, bytes)):
+    unfolded = []
     for item in items:
         if isinstance(item, Iterable) and not isinstance(item, ignore_types):
-            unfold(item, unfolded=unfolded)
+            unfolded.extend(unfold(item))
         else:
             unfolded.append(item)
 
