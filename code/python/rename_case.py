7bbad77439b5b452902df0b4c97e2d8fc75fece7
zousiyu <zousiyu1995@outlook.com>
2018-11-29 22:00:40 +0800

    2018年11月29日，添加python重命名实例

---
+++ code/python/rename_case.py
@@ -0,0 +1,25 @@
+import os
+
+par_dirs = [name for name in os.listdir('.') if os.path.isdir(name)]
+
+
+def rename(par_dir):
+    print("Rename dir [{}] now!".format(par_dir))
+    # get all mp4 file
+    video_files = [
+        video for video in os.listdir(par_dir) if video.endswith('.mp4')
+    ]
+
+    for video_file in video_files:
+        newname = video_file.replace('_Downloadly.ir', '')  # delete something
+        if not video_file.startswith(par_dir):
+            newname = par_dir + ' - ' + video_file  # add par dir name before filename
+        else:
+            print("Pass add dir name")
+        newname = os.path.join(par_dir, newname)
+        oldname = os.path.join(par_dir, video_file)
+        os.rename(oldname, newname)
+
+
+for par_dir in par_dirs:
+    rename(par_dir)
